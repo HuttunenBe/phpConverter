42f067de52f1a08a26c0beb1acf6e04315362411
Huttunenbe <beniittahuttunen@gmail.com>
2025-07-16 20:25:34 +0300

    slight css edits, delete commented out part

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Converter</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+   
 <body>
     <div class="resultCircle">
         <h1>Converter</h1>
@@ -48,37 +50,28 @@
             <form method="POST">
                 <label>Temperature (Celsius):</label>
                 <input type="number" name="temperature"><br>
-                <input type="submit" name="convertTemperature" value="Convert Celcius to °F & K">
+                <input type="submit" name="convertTemperature" value="Convert to °F & K">
             </form>
 
             <form method="POST">
                 <h2>Speed (km/h):</h2>
                 <input type="number" name="speed"><br>
-                <input type="submit" name="convertSpeed" value="Convert Speed to m/s & knots">
+                <input type="submit" name="convertSpeed" value="Convert to m/s & knots">
             </form>
 
             <form method="POST">
                 <label>Mass (kg):</label>
                 <input type="number" name="mass"><br>
-                <input type="submit" name="convertMass" value="Convert Kg to Grams">
+                <input type="submit" name="convertMass" value="Convert to Grams">
             </form>
 
             <form method="POST">
                 <h2>Mass (grams):</h2>
                 <input type="number" name="grams"><br>
-                <input type="submit" name="convertGrams" value="Convert Grams to Kg">
+                <input type="submit" name="convertGrams" value="Convert to Kg">
             </form>
         </div>
 </body>
 
 </html>
 
-<!--if ($_SERVER['REQUEST_METHOD'] === 'POST' (CHECK IF FORM WAS SUBMITTED) && isset($_POST['convertTemperature']) (CHECKS IF BUTTON WAS PUSHED) && $_POST['temperature'] !== '') (CHECKS THAT INPUT IS NOT EMPTY){
-        $tempCelsius = $_POST['temperature']; (THIS STORES THE INPUT!)
-            $fahrenheit = $tempCelsius * 9 / 5 + 32; (VARIABLE STORES THE RESULT)
-            $kelvin = $tempCelsius + 273.15; 
-            
-            DIV OUTSIDE PHP= <div class="result"></div>
-            DIV INSIDE PHP = '<div class="result">''</div>'
-            CAN ALSO WRAP EACH ECHO RESULT IN <P>
-            -->
